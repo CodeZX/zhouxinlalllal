abde85a9a3d970c92e8c0134944f05a9a3afdfa2
周鑫 <15901263220@163.com>
2018-06-09 15:45:42 +0800

    初始化

---
+++ zzzzzz/lllladjflajfljfj/lllladjflajfljfj/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  lllladjflajfljfj
+//
+//  Created by 周鑫 on 2018/6/9.
+//  Copyright © 2018年 tunjin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ zzzzzz/lllladjflajfljfj/lllladjflajfljfj/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  lllladjflajfljfj
+//
+//  Created by 周鑫 on 2018/6/9.
+//  Copyright © 2018年 tunjin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
